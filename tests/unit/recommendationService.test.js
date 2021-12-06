ab34d2a45fdd377952487eee192082422e759022
Leonardo <leonardo.otero390@gmail.com>
2021-12-06 04:40:04 -0400

    feat: test function recommendationService.getRandomSong

--- tests/unit/recommendationService.test.js
+++ tests/unit/recommendationService.test.js
@@ -17,22 +17,60 @@ describe('Get songs ranked by amount', () => {
 
   it('should answer with a array of recommendations', async () => {
     jest.spyOn(songRepository, 'findBestSongs').mockImplementationOnce(() => [
-        {
-          id: 150,
-          name: 'Chitãozinho E Xororó - Evidências',
-          youtubeLink:
-            'https://www.youtube.com/watch?v=ePjtnSPFWK8&ab_channel=CHXVEVO',
-          score: 245,
-        },
-        {
-          id: 12,
-          name: 'Falamansa - Xote dos Milagres',
-          youtubeLink:
-            'https://www.youtube.com/watch?v=ePjtnSPFWK8&ab_channel=CHXVEVO',
-          score: 112,
-        },
-      ]);
+      {
+        id: 150,
+        name: 'Chitãozinho E Xororó - Evidências',
+        youtubeLink:
+          'https://www.youtube.com/watch?v=ePjtnSPFWK8&ab_channel=CHXVEVO',
+        score: 245,
+      },
+      {
+        id: 12,
+        name: 'Falamansa - Xote dos Milagres',
+        youtubeLink:
+          'https://www.youtube.com/watch?v=ePjtnSPFWK8&ab_channel=CHXVEVO',
+        score: 112,
+      },
+    ]);
     const promise = await recommendationService.rankTopSongs(10);
     await expect(promise.length).toBeGreaterThan(0);
+  });
+});
+describe('Get random songs', () => {
+    const song = {
+      id: 150,
+      name: 'Chitãozinho E Xororó - Evidências',
+      youtubeLink: 'https://www.youtube.com/watch?v=ePjtnSPFWK',
+      score: 0,
+    };
+  it('should answer with popular song', async () => {
+      song.score=10;
+    jest
+      .spyOn(songRepository, 'getRandomPopularSong')
+      .mockImplementationOnce(() => song);
+    const promise = await recommendationService.getRandomSong('popular');
+     expect(promise.score).toBeGreaterThan(9);
+  });
+
+  it('should answer with not popular song', async () => {
+    song.score=9;
+    jest
+      .spyOn(songRepository, 'getRandomNotPopularSong')
+      .mockImplementationOnce(() => song);
+    const promise = await recommendationService.getRandomSong('notPopular');
+     expect(promise.score).toBeLessThan(10);
+  });
+
+  it('should answer any song when popular or notPopular arent available', async () => {
+    jest.spyOn(songRepository, 'getRandomPopularSong').mockImplementationOnce(() => false);
+    jest.spyOn(songRepository, 'getRandomSong').mockImplementationOnce(() => song);
+    const promise = await recommendationService.getRandomSong('popular');
+     expect(typeof(promise)).toEqual('object');
+  });
+  it('should Not found', async () => {
+    jest.spyOn(songRepository, 'getRandomPopularSong').mockImplementationOnce(() => false);
+    jest.spyOn(songRepository, 'getRandomSong').mockImplementationOnce(() => false);
+    const promise = recommendationService.getRandomSong('popular');
+    await expect(promise).rejects.toThrowError('Not found');
   });
 });
